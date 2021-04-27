9dc574c88c7f656000485a97f213974c8e218648
Jeremy <robelinjeremy@gmail.com>
2021-04-27 22:44:34 +0200

    ajout class Login

---
+++ WorkSpace/apiGestionConges/src/main/java/com/formationsopra/apigc/entities/Login.java
@@ -0,0 +1,109 @@
+package com.formationsopra.apigc.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "login")
+@SequenceGenerator(name = "seqLogin", sequenceName = "seq_login", initialValue = 10, allocationSize = 1)
+public class Login {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seqLogin")
+	private Integer id;
+	@Email
+	@Column(name = "email", unique = true, nullable = false)
+	private String email;
+	@NotEmpty
+	@Column(name = "password", nullable = false)
+	private String password;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "role", nullable = false)
+	private Role role;
+
+	public Login() {
+		super();
+	}
+
+	public Login(@Email String email, @NotEmpty String password, Role role) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Login(Integer id, @Email String email, @NotEmpty String password, Role role) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
