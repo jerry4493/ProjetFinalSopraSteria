44d0be23c45e574105dc65ea67809a381c6ccb29
Christophe Gallerand <christophe.gallerand@soprasteria.com>
2021-04-27 21:32:02 +0200

    ajout package api

---
+++ WorkSpace/apiGestionConges/src/main/java/com/formationsopra/apigc/config/CacheConfig.java
@@ -0,0 +1,10 @@
+package com.formationsopra.apigc.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
