da5d9a67e3604f20177db823621f7fd73180b823
Jeremy Robelin <robelinjeremy@gmail.com>
2021-04-27 17:29:06 +0200

    creation du projet Spring Boot dans le workspace

---
+++ WorkSpace/apiGestionConges/src/main/java/com/formationsopra/apigc/ApiGestionCongesApplication.java
@@ -0,0 +1,13 @@
+package com.formationsopra.apigc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionCongesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionCongesApplication.class, args);
+	}
+
+}
---
+++ WorkSpace/apiGestionConges/src/test/java/com/formationsopra/apigc/ApiGestionCongesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formationsopra.apigc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionCongesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
