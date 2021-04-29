e463763181c603f69620b6e38be4da275160a8e6
Amandine <amandine.leroy@soprasteria.com>
2021-04-29 09:21:51 +0200

    InscriptonLoginAmandine

--- WorkSpace/webGestionConges/src/app/routes.ts
+++ WorkSpace/webGestionConges/src/app/routes.ts
@@ -1,5 +1,18 @@
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PageIntrouvableComponent } from './page-introuvable/page-introuvable.component';
+import { InscriptionComponent } from './formulaire/inscription/inscription.component';
 
-const routes: Routes = [{}];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: 'inscription', component: InscriptionComponent },
+  { path: '**', component: PageIntrouvableComponent },
+];
 
 export const RoutesRoutes = RouterModule.forChild(routes);
